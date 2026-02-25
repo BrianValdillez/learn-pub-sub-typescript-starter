06581190bfe27bba99699b5b85b3757bb0c43576
Brian Valdillez <valdillez+codebending@alumni.usc.edu>
2026-02-25 11:35:07 -0800

    [2.4] Message Brokers: Connect

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,16 @@
+import amqp from "amqplib";
+
 async function main() {
   console.log("Starting Peril server...");
+
+  const RABBIT_CONNECTION_URL = 'amqp://guest:guest@localhost:5672/';
+  const conn = await amqp.connect(RABBIT_CONNECTION_URL);
+  console.log("Successfully connected to Rabbit Server.")
+
+  process.on('SIGINT', async () => {
+    console.log("Shutting down...");
+    await conn.close();
+  });
 }
 
 main().catch((err) => {
