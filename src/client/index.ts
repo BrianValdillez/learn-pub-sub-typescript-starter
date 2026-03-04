e4504628e7d382a20b506ac21f90f40128bc08a0
Brian Valdillez <valdillez+codebending@alumni.usc.edu>
2026-03-04 11:48:53 -0800

    [3.5] Publishers & Queues: Transient Queues

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,5 +1,21 @@
+import amqp from "amqplib";
+import { declareAndBind, SimpleQueueType } from "../internal/pubsub/pubsub.js";
+import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
+import type { PlayingState } from "../internal/gamelogic/gamestate.js";
+import { clientWelcome } from "../internal/gamelogic/gamelogic.js";
+
 async function main() {
   console.log("Starting Peril client...");
+
+  const RABBIT_CONNECTION_URL = 'amqp://guest:guest@localhost:5672/';
+  const conn = await amqp.connect(RABBIT_CONNECTION_URL);
+  console.log("Successfully connected to Rabbit Server.")
+  
+  const ch = await conn.createConfirmChannel();
+
+  const username = await clientWelcome();
+
+  await declareAndBind(conn, ExchangePerilDirect, `${PauseKey}.${username}`, PauseKey, SimpleQueueType.Transient);
 }
 
 main().catch((err) => {
--- src/internal/pubsub/pubsub.ts
+++ src/internal/pubsub/pubsub.ts
@@ -1,6 +1,11 @@
 import amqp from "amqplib";
 import type { ConfirmChannel } from "amqplib";
 
+export enum SimpleQueueType {
+  Durable,
+  Transient,
+}
+
 export function publishJSON<T>(
   ch: ConfirmChannel,
   exchange: string,
@@ -13,4 +18,23 @@ export function publishJSON<T>(
     ch.publish(exchange, routingKey, bytes, { contentType: "application/json" });
 
     return ch.waitForConfirms();
+}
+
+export async function declareAndBind(
+  conn: amqp.ChannelModel,
+  exchange: string,
+  queueName: string,
+  key: string,
+  queueType: SimpleQueueType,
+): Promise<[amqp.Channel, amqp.Replies.AssertQueue]> {
+  const ch = await conn.createChannel();
+
+  const q = await ch.assertQueue(queueName, {
+    durable: queueType === SimpleQueueType.Durable,
+    autoDelete: queueType === SimpleQueueType.Transient,
+    exclusive: queueType === SimpleQueueType.Transient
+  } );
+  await ch.bindQueue(queueName, exchange, key);
+
+  return [ch, q];
 }
